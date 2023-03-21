dc758e1082acad4dd7695765200f427f0f20aeaf
daniildudka <danilvorona144@gmail.com>
2023-03-21 13:08:57 +0200

    Добавил 3 теста на проверку сообщения

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@
 import time
 
 import pytest
-
+from selenium.webdriver.common.by import By
 from .pages.product_page import ProductPage
 
 
@@ -33,3 +33,35 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.checking_product_name()  # Проверка названия товара
     page.checking_price()  # Проверка совпадения цены
     # time.sleep(10)  # Просто посмотреть
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link_1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3"
+    page = ProductPage(browser,
+                       link_1)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.add_to_basket()  # выполняем метод страницы — добавляем товар в корзину
+    page.solve_quiz_and_get_code()
+    assert page.is_not_element_present(By.CSS_SELECTOR,
+                                       "#messages div:nth-child(1)"), "Success message is presented, but should not be"
+
+
+def test_guest_cant_see_success_message(browser):
+    link_1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3"
+    page = ProductPage(browser,
+                       link_1)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    assert page.is_not_element_present(By.CSS_SELECTOR,
+                                       "#messages div:nth-child(1)"), "Success message is presented, but should not be"
+
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link_1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3"
+    page = ProductPage(browser,
+                       link_1)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.add_to_basket()  # выполняем метод страницы — добавляем товар в корзину
+    page.solve_quiz_and_get_code()
+    assert page.is_disappeared(By.CSS_SELECTOR, "#messages div:nth-child(1)"), "Success message is not disappeared"
