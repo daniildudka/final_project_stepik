71207100043198d9a00def914a764ac9a6486dcb
daniildudka <danilvorona144@gmail.com>
2023-03-21 13:08:33 +0200

    Добавил 2 функции с сообщениями

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,8 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class ProductPage(BasePage):
@@ -24,3 +27,10 @@ class ProductPage(BasePage):
         product_price_in_basket_text = product_price_in_basket.text
         assert product_price_text == product_price_in_basket_text, "Цена не совпадает"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared"
