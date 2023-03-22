d6c7d63c597124a413eadcde25b7050da54817a9
daniildudka <danilvorona144@gmail.com>
2023-03-22 11:39:44 +0200

    Добавил 2 локатора

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators():
     PRODUCT_PRICE = (By.CSS_SELECTOR, '[class="price_color"]')
     PRODUCT_PRICE_IN_BASKET = (By.XPATH, '//*[@id="messages"]/div[3]/div/p[1]/strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
