4aed443157eac1566def8902e4d2fd4c5759a199
daniildudka <danilvorona144@gmail.com>
2023-03-22 11:40:30 +0200

    Добавил 2 теста

--- test_product_page.py
+++ test_product_page.py
@@ -65,3 +65,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket()  # выполняем метод страницы — добавляем товар в корзину
     page.solve_quiz_and_get_code()
     assert page.is_disappeared(By.CSS_SELECTOR, "#messages div:nth-child(1)"), "Success message is not disappeared"
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
