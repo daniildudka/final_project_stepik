404b82357bf38dade9f6213662137d6842083cb2
daniildudka <danilvorona144@gmail.com>
2023-03-27 12:09:27 +0300

    Добавил локаторы для страницы регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -8,6 +8,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    email_input = (By.CSS_SELECTOR, '[id="id_registration-email"]')
+    password1_input = (By.CSS_SELECTOR, '[id="id_registration-password1"]')
+    password2_input = (By.CSS_SELECTOR, '[id="id_registration-password2"]')
+    register_button = (By.CSS_SELECTOR, '[name="registration_submit"]')
+    login_page_link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 
 class ProductPageLocators():
@@ -23,6 +28,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.XPATH, '//*[@id="default"]/header/div[1]/div/div[2]/span/a')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
